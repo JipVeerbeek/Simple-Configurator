aa030fa238d3eb78d16dbccc8187c6cf12ce90b9
Jip Veerbeek <jip.veerbeek@hotmail.nl>
2024-04-30 11:29:00 +0200

    Added models for product and question

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Product, Product_question
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+
+
+class ProductQuestionAdmin(admin.ModelAdmin):
+    list_display = ["id", "product_id", "question_id"]
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Product_question, ProductQuestionAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Product_question(models.Model):
+    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+    question_id = models.ForeignKey('questions.Question', on_delete=models.CASCADE)
