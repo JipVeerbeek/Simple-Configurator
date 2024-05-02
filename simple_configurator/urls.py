5f693f9c331e1544cd3e08f749043e6c36ff7c90
Jip Veerbeek <jip.veerbeek@hotmail.nl>
2024-05-02 16:39:38 +0200

    remove a line

--- simple_configurator/urls.py
+++ simple_configurator/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.contrib import admin
