95f4808559b3cd5b956cb9fc7110d1ec153e5b62
Jip Veerbeek <jip.veerbeek@hotmail.nl>
2024-05-07 11:52:30 +0200

    test serializer

--- products/tests.py
+++ products/tests.py
@@ -2,6 +2,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from products.models import Product
+from products.serializers import ProductSerializer
+
 
 class ProductTests(APITestCase):
     def test_get_product(self):
@@ -14,3 +16,14 @@ class ProductTests(APITestCase):
         self.assertEqual(Product.objects.count(), 2)
         self.assertEqual(response.data[0]['name'], 'Product 1')
         self.assertEqual(response.data[1]['name'], 'Product 2')
+
+    def test_serializer(self):
+        expected_data = [{'id': 1, 'name': 'Product 1'}, {'id': 2, 'name': 'Product 2'}]
+
+        Product.objects.create(name='Product 1')
+        Product.objects.create(name='Product 2')
+
+        queryset = Product.objects.all()
+        serialized_data = ProductSerializer(queryset, many=True).data
+
+        self.assertEqual(serialized_data, expected_data)
