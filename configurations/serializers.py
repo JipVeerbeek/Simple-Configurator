d77ecb689c5c70a55f440079d4a2d9099d68392c
Jip Veerbeek <jip.veerbeek@hotmail.nl>
2024-05-13 16:54:03 +0200

    You can save your answer to a config line now

--- configurations/serializers.py
+++ configurations/serializers.py
@@ -1,7 +1,12 @@
-from .models import Configuration
+from .models import Configuration, ConfigurationLine
 from rest_framework import serializers
 
 class ConfigurationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Configuration
         fields = ['id', 'product_id', 'address_id']
+
+class ConfigurationLineSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConfigurationLine
+        fields = ['id', 'product_question_article_id', 'configuration_id']
--- configurations/views.py
+++ configurations/views.py
@@ -1,7 +1,20 @@
 from rest_framework import generics
 from .models import Configuration
-from .serializers import ConfigurationSerializer
+from .serializers import ConfigurationSerializer, ConfigurationLineSerializer
+from .services import PriceService
+
 
 class ConfigurationCreateView(generics.CreateAPIView):
-    queryset = Configuration.objects.all()
     serializer_class = ConfigurationSerializer
+    queryset = Configuration.objects.all()
+
+
+class AnswerCreateView(generics.CreateAPIView):
+    serializer_class = ConfigurationLineSerializer
+    queryset = Configuration.objects.all()
+
+    def perform_create(self, serializer):
+        created_answer = serializer.save()
+        new_price = PriceService(configuration=created_answer.configuration_id).calculateOrderPrice()
+
+        return new_price
